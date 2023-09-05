116a9fb939e76f01b367993dd4fb0466dedcdb63
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-09-05 09:20:50 -0500

    Add comments to async-example.js

--- 05-hooks-starting-code/async/async-example.js
+++ 05-hooks-starting-code/async/async-example.js
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken';
 
+// ^ the two functions below both generate a JSON Web Token
+
+// * example async fcn using a callback fcn (i.e., doneFn)
 export function generateToken(userEmail, doneFn) {
   jwt.sign({ email: userEmail }, 'secret123', doneFn);
 }
 
+// * example async fcn using a promise
 export function generateTokenPromise(userEmail) {
   const promise = new Promise((resolve, reject) => {
     jwt.sign({ email: userEmail }, 'secret123', (error, token) => {
